36e2c8b5f7339e90506841bc33dd8f16116e2123
kashyapAdi <95838979+kashyapAdi@users.noreply.github.com>
2024-10-12 07:08:15 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -16,4 +16,30 @@
        differential buundling-support older browser
        parcel is a zero config bundler
        tree shaking- parcel will remove unused code 
+
+   kaise karna hai 
+   npm init karo 
+   phir parcel install karo npm install -D parcel
+   phir react karo install
+   waise react dom ko karo
+   phir npx parcel index.html run karo server build ho jayega 
+   lakin ab baar baar nahi likhna npx parcel index.html 
+   to yeh karo 
+   phir package.json me script add karo
+       uske andar start :  parcel index.html
+       build: parcel build index.html 
+       phir ab npm run start karo 
+       waise build ke liye  npm run build karo 
+
+
+       WHAT IS REACT COMPONENT👑?
+       react component is a small reusable piece of code that can be used to build user interface
+       react component is a function that returns JSX
+       react component is a class that extends React.Component
+       react component is a function that returns a virtual dom
+       react component is a small piece of code that can be used to build user interface
        
+       TYPES OF REACT COMPONENT?
+       1) functional component
+       2) class component
+        
--- app.js
+++ app.js
@@ -11,26 +11,94 @@
 react element(object)=html(browser understands)
 
 */
-import React from "react";
-import ReactDOM from "react-dom/client";
+// const parent=React.createElement(
 
-const parent=React.createElement(
-    "div",
-    {id:"parent"},
-   [ React.createElement("div",{id:"child"},[
-    React.createElement("h1",{}," i am a man  "),
-    React.createElement("h2",{}," i am a batman 2"),
-]), React.createElement("div",{id:"child 2"},[
-    React.createElement("h1",{}," i am a batman"),
-    React.createElement("h2",{}," i am a batman 2"),
-])]
-); 
-// isley JSX exit karta ki yeh upar chutiya jo  code hai usko sahi karne ke liye 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
-
-
+//     "div",
+//     {id:"parent"},
+//    [ React.createElement("div",{id:"child"},[
+//     React.createElement("h1",{}," i am a man  ")
+//     React.createElement("h2",{}," i am a batman 2"),
+// ]), React.createElement("div",{id:"child 2"},[
+//     React.createElement("h1",{}," i am a batman"),
+//     React.createElement("h2",{}," i am a batman 2"),
+// ])]
+// );
+//  isley JSX exit karta ki yeh upar chutiya jo  code hai usko sahi karne ke liye
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+//     root.render(parent);
 
 // const heading = React.createElement("h1" ,{id:"heading"} , "hello world ");
 //     const root = ReactDOM.createRoot(document.getElementById("root"));
 //     root.render(heading);
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// todo                          React Element 
+// ?how we create heading in react  ,if we wanted to render elemnent to work in html we use render
+
+// ?const heading = React.createElement("h1", {id:"heading"}, " namaste aditya hu me");
+
+// ?JSX (transpiled before it reaches the js)- parcel-babel : is the java script compiler
+// ?jsx in sab chutiye code ko sahi karega , ab jsx se heaading banaate hai
+//?babel is javscript compiler, it convert jsx to react .create element,it convert diff type of code to different code
+
+// const jsxHeading= <h1 id="heading">namaste aditya hu me using jsx</h1>;
+
+//! this is react element it contain small letter and no arrorw function
+
+// const elem=<span> react element</span>
+// const title= (
+
+//     <h1 className =" head">
+//         {elem}
+//         me hu tera
+//         </h1>
+// );
+
+
+
+//todo  this is React Component because react component have capital letter 
+const Title = ()=> (<h1 class="head">namaste aditya hu me using jsx</h1>);
+const Title1= ()=> (<h1 class ="headd"> namaste me hu avinash</h1>);
+
+// !REACT COMPONENT types
+// ?CLASS BASED COMPONENT-OLD  WAY
+//? FUNCTIONAL COMPONENT -NEW WAY
+  
+
+// todo react functional component
+
+// ?we can write in this method also with return function 
+//  const HeadingComponent=()=> {
+//     return <h1 className ="heading">namaste aditya hu me</h1>;
+// };
+
+const title= (
+
+    <h1 className =" head">
+        me hu tera
+        </h1>
+);
+ 
+// const data = api.getData();
+
+//! component composition:-
+// we can write javascript code using {} in below 
+const number= 10000;
+const HeadingComponent=()=> (
+    <div id ="container">
+        <Title/>
+        <Title1/>
+        {title}
+     <h2>{number}</h2> 
+        <h1 className="heading">namaste  me</h1>
+    </div>
+);
+  
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>);              // render is a function which is used to render react element to html
